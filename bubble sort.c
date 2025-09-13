1fb073eef3e12bc8a06f91dd64a535c9aea5fdc1
Sajal <sajal.cse10.bu@gmail.com>
2025-09-13 21:30:45 +0600

    Add files via upload

---
+++ bubble sort.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+void bubble_sort(int array[],int n)
+{
+    for(int i=0;i<n-1;i++)
+    {
+        for(int j=0;j<n-i-1;j++)
+        {
+            if(array[j]>array[j+1])
+            {
+                int temp=array[j];
+                array[j]=array[j+1];
+                array[j+1]=temp;
+            }
+        }
+    }
+}
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int a[n];
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    bubble_sort(a,n);
+    for(int i=0;i<n;i++)
+    {
+        printf("%d ",a[i]);
+    }
+    printf("\n");
+    return 0;
+}
