07e1c9e6386f56678a8672edf5a77a11b6e113bb
Sajal <sajal.cse10.bu@gmail.com>
2025-09-16 02:00:14 +0600

    Add files via upload

---
+++ count sort.c
@@ -0,0 +1,63 @@
+#include<stdio.h>
+#include<limits.h>
+#include<stdlib.h>
+
+int maximum(int a[],int n)
+{
+    int max=INT_MIN;
+    for(int i=0;i<n;i++)
+    {
+        if(a[i]>max)
+            max=a[i];
+    }
+    return max;
+}
+
+void countsort(int a[],int n)
+{
+    int i,j;
+    int max=maximum(a,n);
+    int* count=(int *) malloc((max+1)*sizeof(int)); //dynamic array
+    for(i=0;i<max+1;i++)
+    {
+        count[i]=0;
+    }
+    for(int i=0;i<n;i++)
+    {
+        count[a[i]]=count[a[i]]+1;
+    }
+    i=0;
+    j=0;
+    while(i<=max)
+    {
+        if(count[i]>0)
+        {
+            a[j]=i;
+            count[i]=count[i]-1;
+            j++;
+        }
+        else
+        {
+            i++;
+        }
+
+    }
+}
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    countsort(arr,n);
+    for(int i=0;i<n;i++)
+    {
+        printf("%d ",arr[i]);
+    }
+    printf("\n");
+    return 0;
+}
+
